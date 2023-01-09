a35bcf3b916b5610e2a3a5281e25023931801458
Justin Fagan <jbfagan7@gmail.com>
2023-01-09 09:00:06 -0500

    feat: updated server + docker + dependencies config

--- server/src/main/java/com/expensejar/server/ServerApplication.java
+++ server/src/main/java/com/expensejar/server/ServerApplication.java
@@ -2,9 +2,17 @@ package com.expensejar.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServerApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
